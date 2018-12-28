ca63c1174b0dc2210ab7c88fc2a73af80495e2e3
Ricardo Sousa <ricardo-svc@live.com>
2018-12-28 09:41:25 -0200

    Position Class

---
+++ ChessGame/board/Position.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace board
+{
+    class Position
+    {
+        public int line { get; set; }
+        public int column { get; set; }
+
+        public Position(int line, int colum)
+        {
+            this.line = line;
+            this.column = colum;
+        }
+        public override string ToString()
+        {
+            return line
+                + " , "
+                + column;
+        }
+    }
+}
