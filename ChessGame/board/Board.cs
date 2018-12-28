36913957219fb13077b814b5ea2057977872bb80
Ricardo Sousa <ricardo-svc@live.com>
2018-12-28 10:11:23 -0200

    Implementing the board

---
+++ ChessGame/board/Board.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace board
+{
+    class Board
+    {
+        public int lines { get; set; }
+        public int columns { get; set; }
+        public Piece[,] piece { get; set; }
+
+        public Board(int lines, int columns)
+        {
+            this.lines = lines;
+            this.columns = columns;
+            piece = new Piece[lines, columns];
+        }
+    }
+}
---
+++ ChessGame/board/Piece.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace board
+{
+    class Piece
+    {
+        public Position position { get; set; }
+        public Color color { get; protected set; }
+        public int qtdeMovimentos { get; protected set; }
+        public Board tab { get; protected set; }     
+
+        public Piece(Position position, Board tab ,Color color)
+        {
+            this.position = position;
+            this.tab = tab;
+            this.color = color;
+            this.qtdeMovimentos = 0;
+        }
+    }
+}
